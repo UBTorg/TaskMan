09e9979ce0953725924392f19907618ccc2e8017
gentianahoxha <gh40407@ubt-uni.net>
2021-03-21 20:59:01 +0100

    Toggle Button

--- taskman/src/App.vue
+++ taskman/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <List title="Task Tracker"/>
+    <List @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>
+    <div v-show="showAddTask">
     <AddTask @add-task="addTask" />
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
   
   </div>
@@ -21,7 +23,9 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask:false,
+
     }
   },
   methods: {
@@ -34,6 +38,9 @@ export default {
     },
     addTask(task){
       this.tasks = [...this.tasks,task]
+    },
+    toggleAddTask(){
+      this.showAddTask= !this.showAddTask
     }
   },
   created() {
--- taskman/src/components/Button.vue
+++ taskman/src/components/Button.vue
@@ -11,7 +11,7 @@ export default{
     },
     methods:{
         onClick(){
-            return;
+            this.$emit('btn-click')
         }
     }
 }

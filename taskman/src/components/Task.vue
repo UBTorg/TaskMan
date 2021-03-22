64d8ec7c4c7c4ce26c8cd0968e89ca8b18f42c5e
Rron Hyseni <rh40241@ubt-uni.net>
2021-03-23 00:46:03 +0100

    Added new ui in main page

--- taskman/src/components/Task.vue
+++ taskman/src/components/Task.vue
@@ -1,5 +1,5 @@
 <template>
-    <div  @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <div class="taskk" @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>
             {{task.text}}
             <i @click="$emit('delete-task',task.id)" class="fas fa-times"></i>
@@ -20,17 +20,28 @@ export default {
 }
 </script>
 <style scoped>
+
 .fas {
-  color: red;
+  color: rgb(78, 78, 78);
 }
+
 .task {
   background: #f4f4f4;
-  margin: 5px;
+  margin: 12px;
   padding: 10px 20px;
   cursor: pointer;
+  min-height: 250px;
+  border-radius: 10px;
+  transition: transform 200ms ease 0s;
+  
 }
+
+.task:hover {
+  transform: translate(2px, -2px);
+}
+
 .task.reminder {
-  border-left: 5px solid green;
+  border-left: 5px solid #55C6F0;
 }
 .task h3 {
   display: flex;
--- taskman/src/components/Tasks.vue
+++ taskman/src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div :key="task.id" v-for="task in tasks">
+    <div class="task-container">
+        <div class="card" :key="task.id" v-for="task in tasks">
             <Task @toggle-reminder="$emit('toggle-reminder', task.id)" 
             @delete-task="$emit('delete-task', task.id)" :task="task"/>
         </div>
@@ -20,3 +20,22 @@ import Task from './Task'
         emits: ['delete-task', 'toggle-reminder']
     }
 </script>
+
+<style scoped>
+.task-container {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+}
+.card {
+  flex: 0 0 33%;
+  cursor: pointer;
+  min-height: 250px;
+}
+
+@media only screen and (min-width: 1500px) {
+    .card {
+        flex: 0 0 25%;
+    }
+}
+</style>
